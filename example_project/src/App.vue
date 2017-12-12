178cc7d3eab72f40954d377ba39064c63d66cc97
Miguel Torres <migueltg93@gmail.com>
2017-12-12 10:28:12 +0100

    changes

--- example_project/src/App.vue
+++ example_project/src/App.vue
@@ -1,23 +1,37 @@
 <template>
-  <div>
+  <div id="main-app">
       <h1>{{title}}</h1>
-        <ul v-for="item in appointments">
+        <ul class="appointment-list" v-for="(item, i) in appointments" :key="i">
           <li>{{item.petName}}</li>
         </ul>
   </div>
 </template>
 
 <script>
-  var apts = [{"petName":"Spot","petOwner":"Constance Smith","aptDate":"2017-07-24 08:30","aptNotes":"This German Shepherd is having some back pain"},{"petName":"Goldie","petOwner":"Barot Bellingham","aptDate":"2017-07-22 15:50","aptNotes":"This Goldfish has some weird spots in the belly"},{"petName":"Mitten","petOwner":"Hillary Goldwyn","aptDate":"2017-07-21 9:15","aptNotes":"Cat has excessive hairballs"},{"petName":"Buffy","petOwner":"Hassum Harrod","aptDate":"2017-07-20 15:30","aptNotes":"This Chihuahua has not eaten for three days and is lethargic"}];
-
   export default {
     name: 'MainApp',
     data() {
       return {
         title: 'Appointment',
-        appointments: apts        
+        theAppointments: []        
       }; //return
-    } //data
+    }, //data
+    created: function() {
+      $.getJSON('./build/appointments.json')
+      .done( info => {
+        this.theAppointments = info;
+      });//getJSON
+    } //created
   }; //default
 
 </script>
+
+<style scoped>
+  .appointment-list {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    margin: 10px 5%;
+    font-size: 1.7 rem;
+  }
+</style>
